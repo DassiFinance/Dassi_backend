963284ef5f558ed2de0deecc013ce5970f303770
Yash Chachad <chachadyash786@gmail.com>
2021-08-23 11:20:01 +0530

    Added model categories

---
+++ controllers/category.js
@@ -0,0 +1,20 @@
+const Category = require("../models/category");
+
+exports.createCategory = async (req, res, next) => {
+  try {
+    const category = new Category(req.body);
+    await category.save();
+    return res.send(category);
+  } catch (error) {
+    return res.send({ error, message: "Could not create category" });
+  }
+};
+
+exports.getCategories = async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+    return res.send(categories);
+  } catch (error) {
+    return res.send({ error, message: "Could not get categories" });
+  }
+};
